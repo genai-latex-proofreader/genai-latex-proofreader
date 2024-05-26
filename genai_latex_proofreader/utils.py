fa15273a44ce90c0032c95747ee164b854fe6b12
Matias Dahl <matias.dahl@iki.fi>
2024-05-26 08:42:24 +0000

    Add utility functions for splitting lists for latex parsing

--- genai_latex_proofreader/utils.py
+++ genai_latex_proofreader/utils.py
@@ -1,7 +1,76 @@
-from typing import Callable, Sequence, TypeVar
+from typing import Callable, Optional, Sequence, TypeVar
 
 A = TypeVar("A")
 B = TypeVar("B")
+
+
+def split_at_first_lambda(
+    xs: Sequence[A], f: Callable[[A], Optional[B]]
+) -> tuple[list[A], Optional[B], list[A]]:
+    """
+    Partition an input list 'xs' at point where 'f' first returns a non-null value.
+
+    Example outputs for xs = [1, 2, 3]
+        [], b, [2, 3]:            f returns b for x=1
+        [1], b, [2, 3]:           f returns b for x=2
+        [1, 2, 3], None, []:      f returns None for all x in xs.
+
+    """
+    for idx, a in enumerate(xs):
+        if (b := f(a)) is not None:
+            return xs[:idx], b, xs[idx + 1 :]
+
+    return list(xs), None, []
+
+
+def split_list_at_lambdas(
+    xs: list[A], split_lambdas: list[Callable[[A], Optional[B]]]
+) -> tuple[list[A], list[tuple[B, list[A]]]]:
+    """
+    Split the list 'xs' at indices where the lambdas in 'split_lambdas'
+    return non-NULL values.
+
+    Args:
+        xs:            The input list to be split.
+        split_lambdas: A list of lambda functions to determine where to split 'xs'.
+
+    Returns:
+        A tuple containing the first part of the split list (from first split), and
+        a list of tuples containing the non-NULL values returned by the rest of
+        the lambdas in 'split_lambdas' and the corresponding split lists.
+
+    Notes: In 'xs', the entries where lambdas are non-NULL must appear in the same
+    order as the lambdas. If this is not the case, the function raises an Exception.
+    """
+    if len(split_lambdas) == 0:
+        return xs, []
+
+    head_lambda, *tail_lambdas = split_lambdas
+    xs_head, b, xs_tail = split_at_first_lambda(xs, head_lambda)
+
+    if b is None:
+        assert xs_tail == []
+        raise Exception(f"split_list_at_lambdas: head_lambda did not return a value")
+
+    xs_tail_head, remaining_splits = split_list_at_lambdas(xs_tail, tail_lambdas)
+    return xs_head, [(b, xs_tail_head), *remaining_splits]
+
+
+def split_list_at_lambda(
+    xs: list[A], split_lambda: Callable[[A], Optional[B]]
+) -> tuple[list[A], list[tuple[B, list[A]]]]:
+    """
+    Same as split_list_at_lambdas, but with a single lambda function that can split
+    input into multiple parts.
+    """
+    xs_head, b, xs_tail = split_at_first_lambda(xs, split_lambda)
+
+    if b is None:
+        assert len(xs_tail) == 0
+        return xs, []
+
+    xs_tail_head, remaining_splits = split_list_at_lambda(xs_tail, split_lambda)
+    return xs_head, [(b, xs_tail_head), *remaining_splits]
 
 
 def compose(*fs: Sequence[Callable[[A], A]]) -> Callable[[A], A]:
@@ -11,7 +80,7 @@ def compose(*fs: Sequence[Callable[[A], A]]) -> Callable[[A], A]:
     Example: compose(f, g, h)(a) = f(g(h(a)))
 
     Args:
-        *fs: A variable number of functions to be composed.
+        *fs: A variable number (n >= 1) of functions to be composed.
 
     Returns:
         A new function that represents the composition of the input functions.
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,11 @@
 import pytest
 
-from genai_latex_proofreader.utils import compose
+from genai_latex_proofreader.utils import (
+    compose,
+    split_at_first_lambda,
+    split_list_at_lambda,
+    split_list_at_lambdas,
+)
 
 
 def test_compose():
@@ -26,3 +31,108 @@ def test_compose_fails_with_zero_input_functions():
 
     with pytest.raises(ValueError):
         compose()
+
+
+# --- test test_split_at_first_lambda ---
+
+
+def test_split_at_first_lambda():
+    def split_after(x0: int) -> int | None:
+        def split(x):
+            if x >= x0:
+                return x
+            return None
+
+        return split
+
+    xs = [1, 2, 3, 4, 3, 2, 1, 100]
+    assert split_at_first_lambda(xs, split_after(0)) == ([], 1, [2, 3, 4, 3, 2, 1, 100])
+    assert split_at_first_lambda(xs, split_after(1)) == ([], 1, [2, 3, 4, 3, 2, 1, 100])
+    assert split_at_first_lambda(xs, split_after(2)) == ([1], 2, [3, 4, 3, 2, 1, 100])
+    assert split_at_first_lambda(xs, split_after(3)) == ([1, 2], 3, [4, 3, 2, 1, 100])
+    assert split_at_first_lambda(xs, split_after(4)) == ([1, 2, 3], 4, [3, 2, 1, 100])
+    assert split_at_first_lambda(xs, split_after(100)) == (
+        [1, 2, 3, 4, 3, 2, 1],
+        100,
+        [],
+    )
+
+    # lambda never returns a non-NULL value -> no split
+    assert split_at_first_lambda(xs, split_after(404)) == (xs, None, [])
+
+
+# --- test split_list_at_lambdas ---
+
+
+def test_split_list_at_lambdas_with_one_entry():
+    # check that when there is one lambda (and split is found), then functions
+    #  - split_at_first_lambda
+    #  - split_list_at_lambdas
+    # return the same result.
+    xs = [1, 2, 3, 4, 5]
+
+    for x in range(1, 6):
+        split_lambda = lambda _x: "split{_x}" if _x == x else None
+        x_pre1, [(lambda_value1, x_post1)] = split_list_at_lambdas(xs, [split_lambda])
+        x_pre2, lambda_value2, y_post2 = split_at_first_lambda(xs, split_lambda)
+        assert x_pre1 == x_pre2
+        assert lambda_value1 == lambda_value2
+        assert x_post1 == y_post2
+
+
+def test_split_list_at_lambdas_multiple_splits():
+    xs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    split_lambdas = [
+        lambda x: "split1" if x == 3 else None,
+        lambda x: "split2" if x == 6 else None,
+        lambda x: "split3" if x == 8 else None,
+        lambda x: "split4" if x == 10 else None,
+    ]
+    assert split_list_at_lambdas(xs, split_lambdas) == (
+        [1, 2],
+        [
+            ("split1", [4, 5]),
+            ("split2", [7]),
+            ("split3", [9]),
+            ("split4", [11]),
+        ],
+    )
+
+
+def test_split_list_at_lambdas():
+    xs = [1, 2, 3]
+
+    # no split found in non-empty list => exception
+    with pytest.raises(Exception):
+        split_list_at_lambdas(xs, [lambda _: None])
+
+    # no lambdas in non-empty list => ok, no split
+    assert split_list_at_lambdas(xs, split_lambdas=[]) == (xs, [])
+
+    # no lambdas, empty list => ok, no split
+    assert split_list_at_lambdas([], split_lambdas=[]) == ([], [])
+
+
+# --- test split_list_at_lambda ---
+
+
+def test_split_list_at_lambda():
+    f = lambda x: "200" if x == 2 else None
+
+    # no match
+    assert split_list_at_lambda([1, 3, 4], f) == ([1, 3, 4], [])
+
+    # multiple matches
+    assert split_list_at_lambda([1, 2, 3, 3, 2, 1, 2], f) == (
+        [1],
+        [("200", [3, 3]), ("200", [1]), ("200", [])],
+    )
+
+    # match first element
+    assert split_list_at_lambda([2, 1, 3], f) == ([], [("200", [1, 3])])
+
+    # match middle element
+    assert split_list_at_lambda([1, 2, 3], f) == ([1], [("200", [3])])
+
+    # match last element
+    assert split_list_at_lambda([1, 3, 2], f) == ([1, 3], [("200", [])])
